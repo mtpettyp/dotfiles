1be062e55dda8dd549f93a2dca53677d031a2558
Mike Pettypiece <mike@streetcontxt.com>
2017-10-10 22:16:25 -0400

    Replace ShiftIt with Hammerspoon equivalent

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,9 +1,25 @@
+-- hotkey mash
+local mash 	 = {"ctrl", "alt"}
+local mash_app 	 = {"cmd", "alt", "ctrl"}
+local mash_shift = {"ctrl", "alt", "shift"}
+
 hs.hotkey.bind({"cmd"}, "e", hs.hints.windowHints)
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "g", hs.grid.toggleShow)
+hs.hotkey.bind(mash_app, "g", hs.grid.toggleShow)
 
 
 -- Grid configuration
 hs.grid.setGrid('2x2')
+
+hs.loadSpoon("WinWin")
+if spoon.WinWin then
+    hs.hotkey.bind(mash_app, "f", function() spoon.WinWin:moveAndResize("fullscreen") end)
+    hs.hotkey.bind(mash_app, "Left", function() spoon.WinWin:moveAndResize("halfleft") end)
+    hs.hotkey.bind(mash_app, "Right", function() spoon.WinWin:moveAndResize("halfright") end)
+    hs.hotkey.bind(mash_app, "Up", function() spoon.WinWin:moveAndResize("halfup") end)
+    hs.hotkey.bind(mash_app, "Down", function() spoon.WinWin:moveAndResize("halfdown") end)
+
+
+end
 
 
 function reloadConfig(files)
